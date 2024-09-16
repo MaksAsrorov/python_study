ce40ba8adb69553a0ed9191a0c14a55d0bdf4119
Макс <maksim.asrorov@gmail.com>
2024-09-17 00:24:13 +0300

    библиотека requests

--- api_tests/chuck.py
+++ api_tests/chuck.py
@@ -1,9 +1,16 @@
 import requests
 
-
 url = "https://api.chucknorris.io/jokes/random"
 print(url)
 
 result = requests.get(url)
 print("Статус код:" +  str(result.status_code))
+assert 200 == result.status_code
+if result.status_code == 200:
+    print("Успешно!")
+else:
+    print("Провал!Запрос ошибочный")
+
+result.encoding = 'utf-8'
+
 print(result.text)
